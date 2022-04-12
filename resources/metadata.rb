03745b37a2cf61035095c047224cfc6ce4cc8d3f
Miguel <manegron@redborder.com>
2022-04-12 09:15:54 +0100

    0.0.5

--- resources/metadata.rb
+++ resources/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'manegron@redborder.com'
 license          'All rights reserved'
 description      'Installs/Configures redborder ips'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.4'
+version          '0.0.5'
 
 depends 'geoip'
 depends 'snmp'
