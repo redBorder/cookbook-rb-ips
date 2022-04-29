dede08a695ac00cf33c2ff3ab703946597ec5ef2
Miguel <manegron@redborder.com>
2022-04-29 12:58:13 +0100

    Addig watchdog

--- resources/recipes/configure.rb
+++ resources/recipes/configure.rb
@@ -19,7 +19,46 @@ ip_regex = /^([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]
 ###monitors_dg   = Chef::DataBagItem.load("rBglobal", "monitors")     rescue monitors_dg={}
 domain_db     = Chef::DataBagItem.load("rBglobal", "publicdomain") rescue domain_db={}
 
+# if domain_db["name"].nil? or domain_db["name"]==""
+#   node.set["redBorder"]["cdomain"] = "redborder.cluster"
+# else
+#   node.set["redBorder"]["cdomain"] = domain_db["name"]
+# end
+
 sensor_id = node["redborder"]["sensor_id"].to_i rescue 0
+
+# managers      = []
+# managers_keys = Chef::Node.list.keys.sort
+# managers_keys.each do |m_key|
+#   m = Chef::Node.load m_key
+#   begin 
+#     roles = m.roles
+#   rescue NoMethodError
+#     roles = []
+#   end
+#   if roles.include?("manager")
+#     managers << m
+#   end
+# end
+
+# managers      = managers.sort{|a,b| (a["rb_time"]||999999999999999999999) <=> (b["rb_time"]||999999999999999999999)}
+
+node.set["redborder"]["chef_client_interval"] = 300
+
+directory "/etc/snortpcaps" do 
+  owner "root" 
+  group "root"
+  mode 0755
+  recursive true
+  action :create
+end
+
+cookbook_file "/etc/snortpcaps/verify.pcap" do
+  source "verify.pcap"
+  owner "root"
+  group "root"
+  mode "0400"
+end
 
 if sensor_id>0
   if node.name.start_with?"rbipscp-"
@@ -29,6 +68,145 @@ if sensor_id>0
   else
     node.run_list(["role[ips-sensor]", "role[rBsensor-#{sensor_id}]", "role[ips-sensor]"])
   end
+end
+
+# virtual_ips = {}
+
+# if resolv_dns_dg["enable"]
+#   virtual_ips_names=["erchef", "kafka", "riak", "rb-webui"]
+#   hasprivate=false
+#   virtual_ips_names.each do |s|
+#     virtual_dg = Chef::DataBagItem.load("rBglobal", "ipvirtual-external-#{s}") rescue ipvirtual_dg =  {}
+#     if !virtual_dg["ip"].nil?  and virtual_dg["ip"]!="" and virtual_dg["ip"] =~ ip_regex 
+#       virtual_ips[s] = virtual_dg["ip"]
+#     else
+#       hasprivate=true
+#     end
+#   end
+  
+#   if hasprivate
+#     # I need to search the first manager with valid IP
+#     ip_manager  = nil
+#     managers.each do |m|
+#       if !m["redBorder"]["manager"][m["redBorder"]["manager"]["management_bond"]].nil? 
+#         if m["redBorder"]["manager"][m["redBorder"]["manager"]["management_bond"]]["ip"] =~ ip_regex
+#           ip_manager=m["redBorder"]["manager"][m["redBorder"]["manager"]["management_bond"]]["ip"]
+#           break
+#         end 
+#       end
+#     end
+  
+#     if !ip_manager.nil?
+#       virtual_ips_names.each do |s|
+#         virtual_ips[s]=ip_manager if virtual_ips[s].nil?
+#       end
+#     elsif managers.size>0
+#       virtual_ips_names.each do |s|
+#         virtual_ips[s]=managers[0]["ipaddress"] if virtual_ips[s].nil?
+#       end
+#     end
+#   end
+
+#   unless virtual_ips.values.include?"127.0.0.1"
+#     template "/etc/hosts" do
+#       source "hosts.erb"
+#       owner "root"
+#       group "root"
+#       mode 0644
+#       retries 2
+#       variables(:virtual_ips => virtual_ips, :managers => managers)
+#     end
+#   end
+# end
+
+# template "/etc/motd" do
+#   source "motd.erb"
+#   owner "root"
+#   group "root"
+#   mode 0644
+#   retries 2
+#   variables(:manager_info => ( virtual_ips["erchef"].nil? ? "erchef.#{node["redBorder"]["cdomain"]}" : virtual_ips["erchef"].to_s ) )
+# end
+
+# CLI Banner configuration
+template "/etc/cli_banner" do
+  source "cli_banner.erb"
+  cookbook "rb-ips"
+  owner "root"
+  owner "root"
+  mode 0644
+  retries 2
+end
+
+template "/etc/chef/role-sensor.json" do
+  source "role-sensor.json.erb"
+  cookbook "rb-ips"
+  owner "root"
+  group "root"
+  mode 0644
+  retries 2
+  variables(:sensor_id => sensor_id)
+end
+
+template "/etc/chef/role-sensor-once.json" do
+  source "role-sensor-once.json.erb"
+  cookbook "rb-ips"
+  owner "root"
+  group "root"
+  mode 0644
+  retries 2
+  variables(:sensor_id => sensor_id)
+end
+
+template "/etc/sudoers.d/redBorder" do
+  source "redBorder.erb"
+  cookbook "rb-ips"
+  owner "root"
+  group "root"
+  mode 0440
+  retries 2
+end
+
+# template "/opt/rb/etc/sysconfig/iptables" do
+#   source "iptables.erb"
+#   owner "root"
+#   group "root"
+#   mode 0644
+#   retries 2
+#   notifies :restart, "service[iptables]"
+# end
+ 
+# directory "/opt/rb/root/.chef" do 
+#   owner "root" 
+#   group "root"
+#   mode 0755
+#   recursive true
+#   action :create
+# end
+
+# template "/root/.chef/knife.rb" do
+#   source "knife.rb.erb"
+#   owner "root"
+#   group "root"
+#   mode 0600
+# end
+
+# template "/etc/yum.repos.d/redBorder-manager.repo" do
+#   source "redBorder-manager.repo.erb"
+#   owner "root"
+#   group "root"
+#   mode 0644
+#   retries 2
+# end
+
+template "/etc/sensor_id" do
+  source "variable.erb"
+  cookbook "rb-ips"
+  owner "root"
+  group "root"
+  mode 0644
+  retries 2
+  variables(:variable => sensor_id )
 end
 
 geoip_config "Configure GeoIP" do
@@ -117,42 +295,53 @@ if node["redborder"]["chef_enabled"].nil? or node["redborder"]["chef_enabled"]
       end # if sensor.id>0
 end # if node
 
-    # rbmonitor_config "Configure redborder-monitor" do
-    #     name node["hostname"]
-    #     monitors monitors_dg["monitors"]
-    #     action ((ips_services["redborder-monitor"] and sensor_id > 0) ? :add : :remove)
-    # end   
-           
-      ###template "/etc/rb_snmp_pass.yml" do
-      ###  source "rb_snmp_pass.yml.erb"
-      ###  cookbook "rb-ips"
-      ###  owner "root"
-      ###  group "root"
-      ###  mode 0755
-      ###  retries 2
-      ###  variables(:monitors => monitors_dg["monitors"])
-      ###  ###notifies :stop, "service[snmptrapd]", :delayed
-      ###  ###notifies :restart, "service[snmpd]", :delayed
-      ###  ###notifies :start, "service[snmptrapd]", :delayed
-      ###end
-      
-      template "/etc/watchdog.conf" do
-        source "watchdog.conf.erb"
-        owner "root"
-        group "root"
-        mode 0644
-        retries 2
-        ###notifies :restart, "service[watchdog]"
-      end
-      
-      ###template "/etc/watchdog.d/020-check-snort.sh" do
-      ###  source "watchdog_020-check-snort.sh.erb"
-      ###  owner "root"
-      ###  group "root"
-      ###  mode 0755
-      ###  retries 2
-      ###  ###notifies :reload, "service[watchdog]", :delayed 
-      ###end
+# rbmonitor_config "Configure redborder-monitor" do
+#     name node["hostname"]
+#     monitors monitors_dg["monitors"]
+#     action ((ips_services["redborder-monitor"] and sensor_id > 0) ? :add : :remove)
+# end   
+        
+  ###template "/etc/rb_snmp_pass.yml" do
+  ###  source "rb_snmp_pass.yml.erb"
+  ###  cookbook "rb-ips"
+  ###  owner "root"
+  ###  group "root"
+  ###  mode 0755
+  ###  retries 2
+  ###  variables(:monitors => monitors_dg["monitors"])
+  ###  ###notifies :stop, "service[snmptrapd]", :delayed
+  ###  ###notifies :restart, "service[snmpd]", :delayed
+  ###  ###notifies :start, "service[snmptrapd]", :delayed
+  ###end
+
+  yum_package "watchdog" do
+    action :upgrade
+    flush_cache [:before]
+  end 
+  
+  template "/etc/watchdog.conf" do
+    source "watchdog.conf.erb"
+    owner "root"
+    group "root"
+    mode 0644
+    retries 2
+    notifies :restart, "service[watchdog]"
+  end
+  
+  template "/etc/watchdog.d/020-check-snort.sh" do
+    source "watchdog_020-check-snort.sh.erb"
+    owner "root"
+    group "root"
+    mode 0755
+    retries 2
+    notifies :reload, "service[watchdog]", :delayed 
+  end
+
+  # [ "/etc/init.d/snortd", "/etc/init.d/barnyard2", "/etc/init.d/bp_watchdog", "/etc/snort", "/etc/pf_ring", "/etc/kdump.conf", "/etc/rb-monitor", "/etc/init.d/rb-monitor", "/etc/rdi", "/etc/watchdog.d" ].each do |l|
+  #   link l do
+  #     to "/opt/rb/#{l}"
+  #   end if !File.exists?"/opt/rb/#{l}"
+  # end
 
 if !node["redborder"]["ipsrules"].nil? and !node["redborder"]["cloud"].nil? 
     node["redborder"]["ipsrules"].to_hash.each do |groupid, ipsrules| 
@@ -176,53 +365,81 @@ if !node["redborder"]["ipsrules"].nil? and !node["redborder"]["cloud"].nil?
     end
 end
 
-template "/etc/cli_banner" do
-    source "cli_banner.erb"
-    cookbook "rb-ips"
-    owner "root"
-    owner "root"
-    mode 0644
-    retries 2
+# service "iptables" do
+#   service_name "iptables"
+#   ignore_failure true
+#   supports :status => true, :reload => false, :restart => true
+#   action([:start])
+# end
+
+# service "rb-monitor" do
+#   service_name "rb-monitor"
+#   ignore_failure true
+#   supports :status => true, :reload => true, :restart => true
+#   sensor_id>0 ? action([:start, :enable]) : action([:stop, :disable])
+# end
+
+service "bp_watchdog" do
+    service_name "bp_watchdog"
+    ignore_failure true
+    supports :status => true, :reload => true, :restart => true
+    node["redborder"]["has_bypass"] ? action([:start, :enable]) : action([:stop, :disable])
 end
 
-template "/etc/chef/role-sensor.json" do
-    source "role-sensor.json.erb"
-    cookbook "rb-ips"
-    owner "root"
-    group "root"
-    mode 0644
-    retries 2
-    variables(:sensor_id => sensor_id)
+service "watchdog" do
+    service_name node[:redborder][:watchdog][:service]
+    ignore_failure true
+    supports :status => true, :reload => true, :restart => true
+    action([:start, :enable])
 end
 
-template "/etc/chef/role-sensor-once.json" do
-    source "role-sensor-once.json.erb"
-    cookbook "rb-ips"
-    owner "root"
-    group "root"
-    mode 0644
-    retries 2
-    variables(:sensor_id => sensor_id)
+# service "chef-client" do
+#     service_name "chef-client"
+#     ignore_failure true
+#     supports :status => true, :reload => true, :restart => true
+#     action([:enable])
+# end
+
+service "sshd" do
+    service_name "sshd"
+    ignore_failure true
+    supports :status => true, :reload => false, :restart => true
+    action [:start, :enable]
 end
 
-template "/etc/sudoers.d/redBorder" do
-    source "redBorder.erb"
-    cookbook "rb-ips"
-    owner "root"
-    group "root"
-    mode 0440
-    retries 2
+execute "force_chef_client_wakeup" do
+    command "/usr/lib/redborder/bin/rb_wakeup_chef"
+    ignore_failure true
+    action ((sensor_id>0) ? :nothing : :run)
 end
 
-template "/etc/sensor_id" do
-    source "variable.erb"
-    cookbook "rb-ips"
-    owner "root"
-    group "root"
-    mode 0644
-    retries 2
-    variables(:variable => sensor_id )
-end
+# if (node["redBorder"]["force-run-once"].nil? or node["redBorder"]["force-run-once"]==false or !node["redBorder"]["force-run-once"])
+#   template "/etc/chef/client.rb" do
+#     source "chef_client.rb.erb"
+#     owner "root"
+#     group "root"
+#     mode 0644
+#     retries 2
+#     variables(:joined => sensor_id>0 )
+#   end
+# else
+#   node.set["redBorder"]["force-run-once"]=false
+# end
+
+# if File.exists?("/etc/yum.repos.d/redBorder.repo")
+#   file "/etc/yum.repos.d/redBorder.repo" do
+#       action :delete
+#   end
+# end
+
+# template "/etc/yum.repos.d/CentOS-Base.repo" do
+#   source "CentOS-Base.repo.erb"
+#   owner "root"
+#   group "root"
+#   mode 0644
+#   retries 2
+#   backup false
+# end
 
 # template "/opt/rb/etc/chef/uptime" do
 #     source "variable.erb"
@@ -260,41 +477,6 @@ end
 #   retries 2
 #   notifies :restart, "service[sshd]", :delayed
 # end
-
-
-# service "bp_watchdog" do
-#     service_name "bp_watchdog"
-#     ignore_failure true
-#     supports :status => true, :reload => true, :restart => true
-#     node["redBorder"]["has_bypass"] ? action([:start, :enable]) : action([:stop, :disable])
-# end
-
-# service "watchdog" do
-#     service_name node[:redBorder][:watchdog][:service]
-#     ignore_failure true
-#     supports :status => true, :reload => true, :restart => true
-#     action([:start, :enable])
-# end
-
-# service "chef-client" do
-#     service_name "chef-client"
-#     ignore_failure true
-#     supports :status => true, :reload => true, :restart => true
-#     action([:enable])
-# end
-
-service "sshd" do
-    service_name "sshd"
-    ignore_failure true
-    supports :status => true, :reload => false, :restart => true
-    action [:start, :enable]
-end
-
-execute "force_chef_client_wakeup" do
-    command "/usr/lib/redborder/bin/rb_wakeup_chef"
-    ignore_failure true
-    action ((sensor_id>0) ? :nothing : :run)
-end
 
 if File.exists?("/etc/init.d/rb-lcd")
     execute "rb-lcd" do
