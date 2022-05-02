a3c6b3b36bb71f6d658b2bae36d7e46119467291
eareyes <eareyes@redborder.com>
2022-05-02 10:28:34 +0100

    Added geoipupdate cron job

--- resources/recipes/configure_cron_tasks.rb
+++ resources/recipes/configure_cron_tasks.rb
@@ -7,3 +7,15 @@ cron_d 'create_clean_logs_daily' do
   ignore_failure true
   command "/usr/lib/redborder/bin/rb_clean_logs_snort.sh"
 end
+
+#--------------------------Geoipupdate-------------------------#
+cron_d 'geoipupdate' do
+  comment "Update GeoIP and GeoLite Databases twice a week"
+  action :create
+  minute '41'
+  hour   '17'
+  weekday '1,4'
+  retries 2
+  ignore_failure true
+  command "/usr/local/bin/geoipupdate -v"
+end
