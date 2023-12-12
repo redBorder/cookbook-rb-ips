c611e752b3dcf5e68b830e3af0264b22a65f0a02
Miguel Negron <manegron@redborder.com>
2023-12-12 10:06:18 +0000

    Add configure_journald

--- resources/recipes/default.rb
+++ resources/recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: ips
 # Recipe:: default
 #
-# Copyright 2022, redborder
+# Copyright 2024, redborder
 #
 # AFFERO GENERAL PUBLIC LICENSE V3
 #
@@ -10,3 +10,4 @@
 include_recipe 'rb-ips::prepare_system'
 include_recipe 'rb-ips::configure'
 include_recipe 'rb-ips::configure_cron_tasks'
+include_recipe 'rb-ips::configure_journald'
