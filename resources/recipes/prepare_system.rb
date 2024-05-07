6c843fbcf9ffda3f3c0d5309ffd527a5fb37fcbf
Luis Blanco <ljblanco@redborder.com>
2024-05-07 11:26:49 +0100

    excluding chef-client

--- resources/recipes/prepare_system.rb
+++ resources/recipes/prepare_system.rb
@@ -64,5 +64,4 @@ node.default["redborder"]["namespaces"] = get_namespaces
 #getting total system memory less 10% reserved by system
 sysmem_total = (node["memory"]["total"].to_i * 0.90).to_i
 #node attributes related with memory are changed inside the function to have simplicity using recursivity
-memory_services(sysmem_total)
-
+memory_services(sysmem_total, ['chef-client'])
