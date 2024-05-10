b02001e73d680791134883fbecf99b46eba84853
Miguel Álvarez <128592227+malvads@users.noreply.github.com>
2024-05-10 15:14:18 +0100

    Update metadata.rb

--- resources/metadata.rb
+++ resources/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'manegron@redborder.com'
 license          'All rights reserved'
 description      'Installs/Configures redborder ips'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.1.10'
 
 depends 'geoip'
 depends 'snmp'
