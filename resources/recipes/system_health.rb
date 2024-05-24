57c69b0d6d4fa3107e6249bb6e8e20a7c75dda9e
Miguel Álvarez <128592227+malvads@users.noreply.github.com>
2024-05-24 17:39:20 +0100

    Update system_health.rb

--- resources/recipes/system_health.rb
+++ resources/recipes/system_health.rb
@@ -1,22 +1,25 @@
 #
 # Cookbook:: ips
-# Recipe:: system_health
+# Recipe:: prepare_system
 #
 # Copyright:: 2024, redborder
 #
 # AFFERO GENERAL PUBLIC LICENSE V3
 #
 
+# Restart barnyard2 service
+execute 'restart_barnyard2' do
+  command 'service barnyard2 restart'
+  action :nothing
+end
+
 # Check services that are not from systemd that should be running
 ruby_block 'check_services_health' do
   block do
-    ['barnyard2'].each do |service|
-      is_enabled = node['redborder']['services'][service]
-      next unless is_enabled
-      unless system("/etc/init.d/#{service} status > /dev/null 2>&1")
-        restart_output = `/etc/init.d/#{service} restart`
-        puts "#{restart_output}"
-      end
+    is_enabled = node['redborder']['services']["barnyard2"]
+    next unless is_enabled
+    unless system("/etc/init.d/barnyard2 status > /dev/null 2>&1")
+      resources(execute: "restart_barnyard2").run_action(:run)      
     end
   end
   action :nothing
