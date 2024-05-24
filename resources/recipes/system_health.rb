1339106f07f208109c036ed5ff9b329fcfa36f68
Miguel Alvarez <malvarez@redborder.com>
2024-05-24 17:03:26 +0100

    Add health check forr non-systemd services

---
+++ resources/recipes/system_health.rb
@@ -0,0 +1,28 @@
+#
+# Cookbook:: ips
+# Recipe:: prepare_system
+#
+# Copyright:: 2024, redborder
+#
+# AFFERO GENERAL PUBLIC LICENSE V3
+#
+
+# Check services that are not from systemd that should be running
+ruby_block 'check_services_health' do
+  block do
+    ['barnyard2'].each do |service|
+      is_enabled = node['redborder']['services'][service]
+      next unless is_enabled
+      unless system("/etc/init.d/#{service} status > /dev/null 2>&1")
+        restart_output = `/etc/init.d/#{service} restart`
+        puts "#{restart_output}"
+      end
+    end
+  end
+  action :nothing
+end
+
+# Run the health check at the end of the chef run
+at_exit do
+  Chef.run_context.resource_collection.find('ruby_block[check_services_health]').run_action(:run)
+end
