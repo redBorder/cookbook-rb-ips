28f7dcc17192a82fce271289c6d0d18c6b714b84
Miguel Álvarez <128592227+malvads@users.noreply.github.com>
2024-08-16 14:06:39 +0100

    Update metadata.rb

--- resources/metadata.rb
+++ resources/metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Eneo Tecnología S.L.'
 maintainer_email 'git@redborder.com'
 license          'AGPL-3.0'
 description      'Installs/Configures redborder ips'
-version          '0.4.1'
+version          '0.5.0'
 
 depends 'rb-common'
 depends 'geoip'
