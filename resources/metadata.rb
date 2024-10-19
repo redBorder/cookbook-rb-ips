381c6c72b805d8cb614c474f8ac2ed9f51f39d33
Miguel Negron <manegron@redborder.com>
2024-10-19 19:25:13 +0100

    Bump version

--- resources/metadata.rb
+++ resources/metadata.rb
@@ -5,7 +5,7 @@ maintainer       'Eneo Tecnología S.L.'
 maintainer_email 'git@redborder.com'
 license          'AGPL-3.0'
 description      'Installs/Configures redborder ips'
-version          '1.2.0'
+version          '2.0.0'
 
 depends 'rb-common'
 depends 'geoip'
