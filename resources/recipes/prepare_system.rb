499881d036ce15e988640421bf9ebf5d870f1d5c
nilsver <nverschaeve@redborder.com>
2025-06-26 14:33:15 +0100

    add check every 24h to clean metadata

--- resources/recipes/prepare_system.rb
+++ resources/recipes/prepare_system.rb
@@ -5,9 +5,10 @@
 
 extend RbIps::Helpers
 
-# Clean metadata to get packages upgrades
-execute 'Clean yum metadata' do
-  command 'yum clean metadata'
+# clean metadata to get packages upgrades, every 24h
+execute 'Clean dnf metadata' do
+  command 'dnf clean metadata && touch /var/cache/dnf/last_makecache'
+  only_if '[ -f /var/cache/dnf/last_makecache ] && [ "$(find /var/cache/dnf/last_makecache -mmin +1440)" ]'
 end
 
 # Stop rb-register
