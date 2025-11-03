ed733b67be6c966983d5a469f02b50c016480b4a
Luis Blanco <108473576+ljblancoredborder@users.noreply.github.com>
2025-11-03 15:15:46 +0000

    Bugfix/#21003 update hosts file (#81)
    * simplify databag logic
    
    * fix typo booolean
    
    * comment deprecated method
    
    * rename confusing variable and resort input variable location
    
    * making code and comments more explanatory
    
    * add bind-utils dependency to use command dig
    
    * lint
    
    * remove unused package
    
    * adding description to code
    
    * remove commented code
    
    * improve code description, improve readability of hosts.erb, read default localhosts entries
    
    * recover manager_to_ip function using resolv
    
    * fix overwrite instead of append
    
    * lint

--- resources/libraries/update_hosts_file.rb
+++ resources/libraries/update_hosts_file.rb
@@ -1,54 +1,53 @@
 module RbIps
   module Helpers
-    require 'resolv'
-
-    def external_databag_services
-      # This function turned too complex ...
-      external_databag = Chef::DataBag.load('rBglobal').keys.grep(/^ipvirtual-external-/)
-      services = external_databag.map { |bag| bag.sub('ipvirtual-external-', '') }
-      services -= %w(sfacctd) # Excluded for IPS
-      services
+    # Gets the list of databags that IPS needs to consider
+    # sfacctd is a external service in the manager not needed by the IPS
+    # Returns the list of databags as an array of strings
+    def fetch_vip_databags
+      databags = []
+      begin
+        databags = Chef::DataBag.load('rBglobal').keys.select { |s| s.include?('ipvirtual-external-') && !s.include?('sfacctd') }
+      rescue e
+        Chef::Log.warn 'No databags found in update hosts.'
+      end
+      databags
     end
 
+    # Map databag-related services to their corresponding IPs in the resolution map
+    # INPUT: The ip of the manager is set by default if the virtual ip isn't declared yet.
+    # Returns a map of <virtual_ip => services[]> with the external vips
     def grouped_virtual_ips(manager_registration_ip)
-      # Hash where services (from databag) are grouped by ip
-      grouped_virtual_ips = Hash.new { |ip, services| ip[services] = [] }
-      # Only those visible for IPS. Manager will recognize these services as nginx
-      # But erchef and s3 are necessary to access VIPs, so moved to implicit
-      nginx_services = %w(http2k webui)
+      ip_services = Hash.new { |ip, services| ip[services] = [] }
 
-      external_databag_services.each do |bag|
-        data = data_bag_item('rBglobal', "ipvirtual-external-#{bag}")
+      vip_databags = fetch_vip_databags
+      vip_databags.each do |bag|
+        data = data_bag_item('rBglobal', bag)
         ip = data['ip'] || manager_registration_ip
-        service_name = bag.delete_prefix('ipvirtual-external-')
+        services =
+          if bag == 'ipvirtual-external-nginx'
+            %w(http2k webui) # Services redirected by nginx
+          else
+            [bag.delete_prefix('ipvirtual-external-')]
+          end
 
-        services = service_name == 'nginx' ? nginx_services : [service_name]
-        grouped_virtual_ips[ip] |= services
+        ip_services[ip] += services
       end
-      grouped_virtual_ips
+      ip_services
     end
 
-    def read_hosts_file
-      hosts_hash = Hash.new { |hash, key| hash[key] = [] }
-      File.readlines('/etc/hosts').each do |line|
-        next if line.strip.empty? || line.start_with?('#')
-        values = line.split(/\s+/)
-        ip = values.shift
-        services = values
-        hosts_hash[ip].concat(services).uniq!
-      end
-      hosts_hash
-    end
-
-    def manager_node_names
-      query = Chef::Search::Query.new
+    # Gets nodes on the format of <hostname>.node of very manager in the cluster.
+    # Returns the names as an array of strings
+    def fetch_node_names
       nodes = []
-      query.search(:node, 'is_manager:true') do |node|
+      Chef::Search::Query.new.search(:node, 'is_manager:true') do |node|
         nodes << "#{node.name}.node"
       end
       nodes
     end
 
+    # Creates relation between localhost and sevices running on it
+    # INPUT: Reference of the ip=>domains map on build
+    # Returns the reference of the map with the additional info
     def add_localhost_info(hosts_info)
       hosts_info['127.0.0.1'] = {}
 
@@ -60,17 +59,31 @@ module RbIps
       hosts_info
     end
 
+    # Adds to input map nodes in consideration: manager nodes and the proper IPS node.
+    # Also we add the general cdomain.
+    # INPUT:
+    #   hosts_info: Reference of the ip=>domains map on build
+    #   manager_registration_ip: The ip of the manager where the IPS was registered
+    #   cdomain: The cdomain defined on installation of the manager/cluster
+    # Returns the reference of the map with the additional info
     def add_manager_names_info(hosts_info, manager_registration_ip, cdomain)
       hosts_info[manager_registration_ip] = {}
-      intrusion_node_name = "#{node.name}.node"
-      node_names = manager_node_names << intrusion_node_name # append
+      ips_node_name = "#{node.name}.node"
+      manager_node_names = fetch_node_names
+      node_names = manager_node_names << ips_node_name # append
       hosts_info[manager_registration_ip]['node_names'] = node_names
       hosts_info[manager_registration_ip]['cdomain'] = cdomain
       hosts_info
     end
 
-    # Services not contained in node information
-    # Chef needs to know these services to resolve them properly on the first chef run.
+    # Adds the services needed to make cookbooks like this apply the domain resolution
+    # properly. Notice that this function depends on the implicit resolution of
+    # erchef and s3 are necessary, so that's why we add them to keep them on the update.
+    # INPUT:
+    #   hosts_info: Reference of the ip=>domains map on build
+    #   manager_registration_ip: The ip of the manager where the IPS was registered
+    #   cdomain: The cdomain defined on installation of the manager/cluster
+    # Returns the reference of the map with the additional info
     def add_manager_services_info(hosts_info, manager_registration_ip, cdomain)
       implicit_services = [
         "erchef.#{cdomain}",
@@ -79,7 +92,6 @@ module RbIps
         "erchef.service.#{cdomain}",
         's3.service',
         "s3.service.#{cdomain}",
-        ###
       ]
 
       other_services = ['data', 'rbookshelf.s3'].map { |s| "#{s}.#{cdomain}" } # On deprecation.
@@ -87,11 +99,19 @@ module RbIps
       hosts_info
     end
 
+    # Adds the external services that can have virtual ips. If no virtual ip is assigned
+    # yet, the manager ip would be used by default.
+    # INPUT:
+    #   hosts_info: Reference of the ip=>domains map on build
+    #   manager_registration_ip: The ip of the manager where the IPS was registered
+    #   cdomain: The cdomain defined on installation of the manager/cluster
+    # Returns the reference of the map with the additional info
     def add_virtual_ips_info(hosts_info, manager_registration_ip, cdomain)
       is_mode_manager = !node['redborder']['cloud']
-      grouped_virtual_ips(manager_registration_ip).each do |ip, services|
+      ip_services = grouped_virtual_ips(manager_registration_ip)
+
+      ip_services.each do |ip, services|
         services.each do |service|
-          # Add running services to localhost
           next if ip == '127.0.0.1'
 
           target_ip = ip && is_mode_manager ? ip : manager_registration_ip
@@ -102,32 +122,38 @@ module RbIps
           # ... But some services might be pointing to these wrong domains. Need to investigate.
           hosts_info[target_ip]['services'] << "#{service}.service"
           hosts_info[target_ip]['services'] << "#{service}.service.#{cdomain}"
-          ###
         end
       end
       hosts_info
     end
 
+    # Main function that creates a ruby logic hash map of ip => domains to be reading by
+    # /etc/hosts template. The function is subdivided based on the logic location
+    # and the nature of the domain, some are services in localhost, others node names.
+    # Returns the complete map that is going to be put in /etc/hosts in hash map format,
+    # where keys are the ips and values are the domains in array of strings format.
     def update_hosts_file
+      hosts_info = Hash.new { |_ip, _domains| }
+
       unless node.dig('redborder', 'resolve_host')
         domain_name = node.dig('redborder', 'manager_registration_ip')
-        return {} if domain_name.nil?
+        return hosts_info if domain_name.nil?
 
         resolved_ip = manager_to_ip(domain_name)
-        return {} if resolved_ip.nil?
+        return hosts_info if resolved_ip.nil?
 
         node.normal['redborder']['resolve_host'] = resolved_ip
       end
       manager_registration_ip = node.dig('redborder', 'resolve_host')
-      return {} unless manager_registration_ip
+      return hosts_info unless manager_registration_ip
 
       cdomain = node.dig('redborder', 'cdomain')
 
-      hosts_info = {}
-      hosts_info = add_localhost_info(hosts_info)
-      hosts_info = add_manager_names_info(hosts_info, manager_registration_ip, cdomain)
-      hosts_info = add_manager_services_info(hosts_info, manager_registration_ip, cdomain)
-      add_virtual_ips_info(hosts_info, manager_registration_ip, cdomain) # returns hosts_info
+      add_localhost_info(hosts_info)
+      add_manager_names_info(hosts_info, manager_registration_ip, cdomain)
+      add_manager_services_info(hosts_info, manager_registration_ip, cdomain)
+      add_virtual_ips_info(hosts_info, manager_registration_ip, cdomain)
+      hosts_info
     end
   end
 end
