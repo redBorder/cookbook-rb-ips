c0f877b396badbfb79f93f6984ea50030b3d2d03
ljblancoredborder <ljblanco@redborder.com>
2025-11-03 11:16:50 +0000

    adding description to code

--- resources/libraries/update_hosts_file.rb
+++ resources/libraries/update_hosts_file.rb
@@ -1,18 +1,20 @@
 module RbIps
   module Helpers
-    # Returns the list of databags IPS needs to consider, in format []
+    # Gets the list of databags related with external services in interest
+    # Returns the list of databags the IPS needs to consider, in format []
     def fetch_vip_databags
       databags = []
       begin
         databags = Chef::DataBag.load('rBglobal').keys.select { |s| s.include?('ipvirtual-external-') && !s.include?('sfacctd') }
       rescue e
-        puts 'WARNING: No databags found in update hosts.'
+        Chef::Log.warn 'No databags found in update hosts.'
       end
       databags
     end
 
+    # Map databag-related services to their corresponding IPs in the resolution map
+    # INPUT: The ip of the manager is set by default if the virtual ip isn't declared yet.
     # Returns a map of <virtual_ip => services[]> with the external vips
-    # By default ip is not virtual, is just the manager_registration ip
     def grouped_virtual_ips(manager_registration_ip)
       ip_services = Hash.new { |ip, services| ip[services] = [] }
 
@@ -44,6 +46,8 @@ module RbIps
     #   hosts_hash
     # end
 
+    # Gets nodes on the format of <hostname>.node of very manager in the cluster.
+    # Returns the names as an array of strings
     def fetch_node_names
       nodes = []
       Chef::Search::Query.new.search(:node, 'is_manager:true') do |node|
@@ -52,7 +56,9 @@ module RbIps
       nodes
     end
 
-    # Adds to input map services running in the IPS
+    # Creates relation between localhost and sevices running on it
+    # INPUT: Reference of the ip=>domains map on build
+    # Returns the reference of the map with the additional info
     def add_localhost_info(hosts_info)
       hosts_info['127.0.0.1'] = {}
 
@@ -64,20 +70,30 @@ module RbIps
       hosts_info
     end
 
-    # Adds to input map nodes in consideration: managers and self
+    # Adds to input map nodes in consideration: managers and the proper IPS
+    # INPUT:
+    #   hosts_info: Reference of the ip=>domains map on build
+    #   manager_registration_ip: The ip of the manager where the IPS was registered
+    #   cdomain: The cdomain defined on installation of the manager/cluster
+    # Returns the reference of the map with the additional info
     def add_manager_names_info(hosts_info, manager_registration_ip, cdomain)
       hosts_info[manager_registration_ip] = {}
-      intrusion_node_name = "#{node.name}.node"
+      ips_node_name = "#{node.name}.node"
       manager_node_names = fetch_node_names
-      node_names = manager_node_names << intrusion_node_name # append
+      node_names = manager_node_names << ips_node_name # append
       hosts_info[manager_registration_ip]['node_names'] = node_names
       hosts_info[manager_registration_ip]['cdomain'] = cdomain
       hosts_info
     end
 
-    # Adds critical services to the input hosts_info map. They are critical because
-    # chef needs to know these services to resolve them properly on the first chef run.
-    # Erchef and s3 are necessary to access VIPs.
+    # Adds the services needed to make cookbooks like this apply the domain resolution
+    # properly. Notice that this function depends on the implicit resolution of
+    # erchef and s3 are necessary, so that's why we add them to keep them on the update.
+    # INPUT:
+    #   hosts_info: Reference of the ip=>domains map on build
+    #   manager_registration_ip: The ip of the manager where the IPS was registered
+    #   cdomain: The cdomain defined on installation of the manager/cluster
+    # Returns the reference of the map with the additional info
     def add_manager_services_info(hosts_info, manager_registration_ip, cdomain)
       implicit_services = [
         "erchef.#{cdomain}",
@@ -93,13 +109,19 @@ module RbIps
       hosts_info
     end
 
+    # Adds the external services that can have virtual ips. If no virtual ip is assigned
+    # yet, the manager ip would be used by default.
+    # INPUT:
+    #   hosts_info: Reference of the ip=>domains map on build
+    #   manager_registration_ip: The ip of the manager where the IPS was registered
+    #   cdomain: The cdomain defined on installation of the manager/cluster
+    # Returns the reference of the map with the additional info
     def add_virtual_ips_info(hosts_info, manager_registration_ip, cdomain)
       is_mode_manager = !node['redborder']['cloud']
       ip_services = grouped_virtual_ips(manager_registration_ip)
 
       ip_services.each do |ip, services|
         services.each do |service|
-          # Add running services to localhost
           next if ip == '127.0.0.1'
 
           target_ip = ip && is_mode_manager ? ip : manager_registration_ip
@@ -115,9 +137,11 @@ module RbIps
       hosts_info
     end
 
-    # Main function where we define a logic ruby hash map to be reading by
-    # /etc/hosts template. The fucntion is subdivided based on the logic location
-    # and the nature of the domain, some are services in localhost, others node names
+    # Main function that creates a ruby logic hash map of ip => domains to be reading by
+    # /etc/hosts template. The function is subdivided based on the logic location
+    # and the nature of the domain, some are services in localhost, others node names.
+    # Returns the complete map that is going to be put in /etc/hosts in hash map format,
+    # where keys are the ips and values are the domains in array of strings format.
     def update_hosts_file
       hosts_info = Hash.new { |_ip, _domains| }
 
@@ -135,10 +159,11 @@ module RbIps
 
       cdomain = node.dig('redborder', 'cdomain')
 
-      hosts_info = add_localhost_info(hosts_info)
-      hosts_info = add_manager_names_info(hosts_info, manager_registration_ip, cdomain)
-      hosts_info = add_manager_services_info(hosts_info, manager_registration_ip, cdomain)
-      add_virtual_ips_info(hosts_info, manager_registration_ip, cdomain) # returns hosts_info
+      add_localhost_info(hosts_info)
+      add_manager_names_info(hosts_info, manager_registration_ip, cdomain)
+      add_manager_services_info(hosts_info, manager_registration_ip, cdomain)
+      add_virtual_ips_info(hosts_info, manager_registration_ip, cdomain)
+      hosts_info
     end
   end
 end
