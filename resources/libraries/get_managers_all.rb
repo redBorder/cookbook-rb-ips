e11799d0ccc0cd39527b100799a2c2aee9cfe3a1
manegron <manegron@redborder.com>
2026-01-30 13:15:49 +0000

    Add rescue

--- resources/libraries/get_managers_all.rb
+++ resources/libraries/get_managers_all.rb
@@ -4,7 +4,12 @@ module RbIps
       managers = []
       managers_keys = Chef::Node.list.keys.sort
       managers_keys.each do |m_key|
-        m = Chef::Node.load(m_key)
+        m = nil
+        begin
+          m = Chef::Node.load(m_key)
+        rescue
+          Chef::Log.error("[get_managers_all] Failed to load node: #{m_key}")
+        end
         roles = m[:roles] || []
         if roles.include?('manager')
           managers << m
